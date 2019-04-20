9d062386d15b3e9dc40480efd56ee16374b70b87
NaNa Mathis <nanarhmathis@gmail.com>
2019-04-19 19:16:15 -0700

    linked css to each html

--- app/routes.py
+++ app/routes.py
@@ -102,5 +102,3 @@ app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-
