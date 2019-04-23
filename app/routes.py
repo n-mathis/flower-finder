45ee8626e06127a033bc88ac13c47adeaf9c5256
ElizabethRiffle <elizabeth@wa-inc.com>
2019-04-22 22:51:22 -0700

    Another one

--- app/routes.py
+++ app/routes.py
@@ -47,7 +47,7 @@ def upload_file():
 
             ##output = output.toList()
 
-            ouput_list = [element.item() for element in output.flatten()]
+            output_list = [element.item() for element in output.flatten()]
 
 
             classes = ['alpineseaholly', 'anthurium', 'artichoke', 'azalea', 'ballmoss', 'balloonflower', \
