cbb4e8322ff6286b85a96c9c1756755d93781129
ElizabethRiffle <elizabeth@wa-inc.com>
2019-04-28 15:34:11 -0700

    Deleted everything

--- setup/restart.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo systemctl daemon-reload
-sudo systemctl start cs121
-sudo systemctl restart cs121
-sudo systemctl enable cs121
-sudo systemctl start nginx
-sudo systemctl restart nginx
--- setup/wsgi.py
+++
@@ -1,4 +0,0 @@
-from cs121 import app
-  
-if __name__ == "__main__":
-    app.run()
