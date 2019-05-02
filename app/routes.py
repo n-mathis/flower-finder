334e31aeaa34ddd0d66102c0d62ca3c74f859adc
NaNa Mathis <nanarhmathis@gmail.com>
2019-05-01 21:19:52 -0700

    working on database

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def getData():
     return render_template('database.html'. species=species,name=name,gen_info=gen_info, 
              lifecycle = lifecycle, difficulty = difficulty, planting = planting, watering = watering, seasons = seasons, 
              warnings = warnings, sunlight = sunlight, soil = soil, website = website, citation = citation)
-    return jsonify({gen_info})
+    # return jsonify({gen_info})
     # return (''), 204
 
 def flowerInfo(prediction):
