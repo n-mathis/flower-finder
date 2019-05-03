e6433f19d23aa43dd5450a372f0c7022d99a18ce
NaNa Mathis <nanarhmathis@gmail.com>
2019-05-03 04:20:34 -0700

    edited routes to reflect file name change

--- app/routes.py
+++ app/routes.py
@@ -139,7 +139,7 @@ def database():
         warnings = flower[8].split('\n')
         website = flower[11]
         citation = flower[12]
-        return render_template('moreinfo.html',
+        return render_template('moreInfo.html',
                 predimg=predimg, name=name, gen_info=gen_info, lifecycle = lifecycle, difficulty = difficulty, planting = planting, watering = watering, seasons = seasons, 
                 warnings = warnings, sunlight = sunlight, soil = soil, website = website, citation = citation)
     return render_template('database.html')
